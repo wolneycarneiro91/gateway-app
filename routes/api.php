36e5b1df161254e4638cfa131317be0205af4dd7
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2022-03-05 21:28:09 -0300

    Adicionando rotas de api secundária

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Utils\DefaultResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/casas',function(DefaultResponse $defaultResponse, Request $request){    
+    $response = Http::withHeaders([
+        'Accept'=>'application/json',        
+    ])->get(config('microservices.avaliable.micro_01.url').'/api/casas',$request->all());    
+    return $defaultResponse->response($response);//->json(json_decode($response->body()),$response->status());
+});
+
+Route::get('/casas/{id}',function(DefaultResponse $defaultResponse, Request $request){    
+    $response = Http::withHeaders([
+        'Accept'=>'application/json',        
+    ])->get(config('microservices.avaliable.micro_01.url').'/api/casas/'.$request->id);   
+   // dd($response);
+    return $defaultResponse->response($response);//->json(json_decode($response->body()),$response->status());
+});
+
+Route::post('/casas',function(DefaultResponse $defaultResponse, Request $request){    
+    $response = Http::withHeaders([
+        'Accept'=>'application/json',        
+    ])->post(config('microservices.avaliable.micro_01.url').'/api/casas',$request->all());    
+    return $defaultResponse->response($response);//->json(json_decode($response->body()),$response->status());
+});
+
 
 
 Route::get('/',function(){
